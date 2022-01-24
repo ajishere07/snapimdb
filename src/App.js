32ef278bba2ac3231978c55197f5fbd968ba5f5a
Anuj Yadav <anujpy.acct@gmail.com>
2022-01-24 12:48:37 +0530

    App-page structure done with favicon,header and footer

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Home from "./components/home/Home";
 import PageNotFound from "./components/pageNotFound/PageNotFound";
 import Footer from "./components/footer/Footer";
 import MovieDetail from "./components/movieDetail/MovieDetail";
+import Header from "./components/header/Header";
 function App() {
   return (
     <div className="App">
-      <Headers />
-      <Route>
-        <Route path="/" exact element={<Home />}></Route>
-        <Route path="/movie/:imdbID" element={<MovieDetail />}></Route>
-        <Route element={<PageNotFound />}></Route>
-      </Route>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/movie/:imdbID" element={<MovieDetail />}></Route>
+          <Route element={<PageNotFound />}></Route>
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import userico from "../../assets/images/useico.png";
+import Logo from "../../assets/images/Logo2.png";
+import "./Header.scss";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="navContainer">
+        <Link to="/" className="BrandContainer">
+          <img src={Logo} alt="logo" className="logoImg" />
+          <h1 className="logo">SnapMovies</h1>
+        </Link>
+        <div className="user-image">
+          <img src={userico} alt="user" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
