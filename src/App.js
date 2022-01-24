7736a11621a61c91101977da52162c4aafa00e75
Anuj Yadav <anujpy.acct@gmail.com>
2022-01-24 10:06:03 +0530

    API Setup and Routing Setup 🚧

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
-
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import PageNotFound from "./components/pageNotFound/PageNotFound";
+import Footer from "./components/footer/Footer";
+import MovieDetail from "./components/movieDetail/MovieDetail";
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Headers />
+      <Route>
+        <Route path="/" exact element={<Home />}></Route>
+        <Route path="/movie/:imdbID" element={<MovieDetail />}></Route>
+        <Route element={<PageNotFound />}></Route>
+      </Route>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
