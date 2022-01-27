d2f9000d0b744044d8983cec32acd9726fd64849
Anuj Yadav <anujpy.acct@gmail.com>
2022-01-27 12:52:12 +0530

    REDUX MIDDELWARE: FETCHING MOVIES✨

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,15 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API_Key } from "../../apis/movieAPI_KEY";
+import movieAPI from "../../apis/movieAPI";
+const movieText = "Harry";
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const res = await movieAPI
+      .get(`?apiKey=${API_Key}&s=${movieText}&type=movie`)
+      .catch((e) => {
+        console.log("Error:", e);
+      });
+    return res.data;
+  }
+);
 
 const initialState = {
-  movies: [],
+  movies: {},
 };
 
 const movieSlice = createSlice({
   name: "movies",
   initialState,
-  reducers: {
-    addMovie: (state, { payload }) => {
-      state.movies = payload;
+
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log("pending");
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("fulfilled ");
+
+      return { ...state, movies: payload };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("rejected");
     },
   },
 });
